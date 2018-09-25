cd9b8c9801e56bc98ff6fa98fc4169602edc1644
Trung <>
2018-09-25 08:28:19 +0700

    update 26/9/18

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+//import UIKit
+//var a: NSMutableArray = [2]
+//var b: NSArray = [6]
+//var b = 7
+//var c = 0
+//var a: Int {
+//    get {
+//        b = b + 5
+//        return b
+//    }
+//
+//    set {
+//    c = a + 5
+//    }
+//}
+// print(a)
+//a = b
+//print(c)
+
+//  remove nguyen am
+//var stringData = "atrunghiendd"
+//var new = ""
+//
+//
+//func remove(charterData: Character) -> Bool {
+//
+//    let ueoai = "ueoaiOAI"
+//        for i in ueoai {
+//            if i == charterData {
+//                return false
+//            }
+//         }
+//    return true
+//}
+//for i in stringData {
+//    if remove(charterData: i) == true {
+//        new.append(i)
+//    }
+//}
+//print(new)
+
